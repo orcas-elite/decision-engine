0fb4de7ef0a70f476fbd90ea1565b2c80e1b4950
Niklas Kammhoff <niklas@kammhoff.com>
2019-05-28 23:08:26 +0200

    Add results for noqlearning

--- decisionengine/algorithms.py
+++ decisionengine/algorithms.py
@@ -2,6 +2,7 @@ import random
 import operator 
 import numpy as np 
 from sklearn import neural_network
+from queue import *
 
 class BaseAlgorithm(object):
     def __init__(self, arch,params):
@@ -16,13 +17,125 @@ class BaseAlgorithm(object):
             for fault in fault_types: 
                 self.fault_injections.append(operation.name + '-' + fault) 
 
-    def get_action(self):
+    def get_experiment(self):
         return 0
 
     def result(self, result):
         pass
     def reset(self):
         self.__init__(self.arch,self.params)
+
+class BayesianLowAlgorithm(BaseAlgorithm):
+    def __init__(self,arch,params):
+        super(BayesianLowAlgorithm, self).__init__(arch,params)
+        self.name = 'bayes-low'
+        self.counters = []
+        self.probs = []
+        self.fault_injections = []
+        self.train_mode = True 
+        self.iterations = 0
+        self.training_limit = 100
+
+        operations = self.arch.get_operations()
+        faults = ['abort','delay']
+        for op in operations:
+            if op.name not in ['a1','a2']:
+                for fault in faults:
+                    self.fault_injections.append(op.name + '-' + fault)
+                    self.counters.append([0,0])
+                    self.probs.append(1/((2*len(operations))-4))
+
+    def get_experiment(self):
+        choice = np.random.choice(range(len(self.fault_injections)), 1, replace=True, p=self.probs)
+        self.last_injection = self.fault_injections[choice[0]]
+        self.last_index = choice[0]
+
+        self.iterations += 1
+        if self.iterations >= self.training_limit: 
+            self.train_mode = False
+        return self.last_injection
+
+    def result(self,result):
+        if result > 0:
+            self.counters[self.last_index][0] += 1
+        self.counters[self.last_index][1] += 1
+
+        if not self.train_mode:
+            if self.counters[self.last_index][0] > 0:
+                self.probs[self.last_index] = self.counters[self.last_index][0] / self.counters[self.last_index][1]
+                sum_probs = sum(self.probs)
+                for i in range(len(self.probs)):
+                    self.probs[i] = self.probs[i] / sum_probs
+class BayesianHighAlgorithm(BaseAlgorithm):
+    def __init__(self,arch,params):
+        super(BayesianHighAlgorithm, self).__init__(arch,params) 
+        self.name = 'bayes-high'
+        self.feature_list = []
+        feature_init = []
+        self.fault_injections = {}
+        self.counters = []
+        self.probs = []
+        self.train_mode = True 
+        self.iterations = 0
+        self.training_limit = 100
+
+        seen = []
+        operations = self.arch.get_operations() 
+        faults = ['abort','delay']
+        for op in operations:
+            if op.name not in ['a1','a2']:
+                for fault in faults:
+                    features = self.extract_features(op,fault)
+                    if features not in feature_init:
+                        feature_init.append(features)
+                    self.fault_injections[op.name + '-' + fault] = features 
+        
+
+        for feature in feature_init:
+            self.feature_list.append(feature)
+            self.counters.append([0,0])
+            self.probs.append(1/len(feature_init)) 
+        
+    def get_experiment(self):
+        choice = np.random.choice(range(len(self.feature_list)), 1, replace=True, p=self.probs) 
+        all_matches = [] 
+        for key,value in self.fault_injections.items():
+            if value == self.feature_list[choice[0]]:
+                all_matches.append(key)
+        
+        self.last_injection = random.choice(all_matches)
+        self.last_index = choice[0]
+
+        self.iterations += 1
+        if self.iterations >= self.training_limit: 
+            self.train_mode = False
+        return self.last_injection
+
+    def result(self,result):
+        if result > 0:
+            self.counters[self.last_index][0] += 1
+        self.counters[self.last_index][1] += 1
+
+        if not self.train_mode:
+            if self.counters[self.last_index][0] > 0:
+                self.probs[self.last_index] = self.counters[self.last_index][0] / self.counters[self.last_index][1]
+                sum_probs = sum(self.probs)
+                for i in range(len(self.probs)):
+                    self.probs[i] = self.probs[i] / sum_probs
+
+    def extract_features(self,op,fault):
+        features = [] 
+        if fault == 'abort':
+            features.append(0)
+        else:
+            features.append(1) 
+        if op.circuitbreaker == None:
+            features.append(0)
+        else:
+            features.append(1)
+        features.append(len(op.dependencies)) 
+
+        return features
 
 class BanditEpsilonAlgorithm(BaseAlgorithm):
     def __init__(self,arch,params):
@@ -32,13 +145,13 @@ class BanditEpsilonAlgorithm(BaseAlgorithm):
         self.min_epsilon = params[2]
         self.original_epsilon = self.epsilon
 
-        self.name = 'bandit-epsilon'
+        self.name = 'bandit-eps'
         self.Q = {}
         self.N = {}
         for action in self.fault_injections:
             self.Q[action] = 0
             self.N[action] = 1
-    def get_action(self):
+    def get_experiment(self):
         if random.random() < self.epsilon:
             action = random.choice(list(self.Q.keys())) 
             self.last_action = action
@@ -62,13 +175,13 @@ class BanditOptimisticAlgorithm(BaseAlgorithm):
         super(BanditOptimisticAlgorithm, self).__init__(arch, params)
         self.optimistic = params[0]
 
-        self.name = 'bandit-optimistic'
+        self.name = 'bandit-opt'
         self.Q = {}
         self.N = {}
         for action in self.fault_injections:
             self.Q[action] = self.optimistic
             self.N[action] = 1
-    def get_action(self):
+    def get_experiment(self):
         # TODO: GEt all the max actions, selection randomly from all 
         action = max(self.Q.items(), key=operator.itemgetter(1))[0]
         self.last_action = action
@@ -110,7 +223,7 @@ class QLearningAlgorithm(BaseAlgorithm):
                 self.states[operation.name]['N'][dep.name] = 0
 
         self.state = random.choice(list(self.states.keys()))
-    def get_action(self):
+    def get_experiment(self):
         if np.random.rand() >= self.epsilon:
             action = self.random_argmax(self.states[self.state]['Q'])
         else:
@@ -154,36 +267,43 @@ class TableauAlgorithm(BaseAlgorithm):
         self.min_epsilon = params[3]
         self.action_size = params[4]
 
-
+        self.fault_injections = {}
         self.states = {}
         operations = self.arch.get_operations()
         faults = ['abort','delay']
         for operation in operations:
             if operation.name not in ['a1','a2']:
                 for fault in faults:
-                    self.states[operation.name + '-' + fault] = {
+                    features = self.extract_features(operation,fault)
+                    self.states[str(features)] = {
                         'Q': [self.initial_q] * self.action_size,
+                        # initialize N at 1 to prevent the first result setting Q to 0 
                         'N': [1] * self.action_size
                     }
-    def get_action(self):
-        if np.random.rand() >= self.epsilon:
-            queues = []
-            for i in range(self.action_size):
-                queues.append([])
-            for s in self.states:
+                    self.fault_injections[operation.name + '-' + fault] = str(features)
+                    
+    def get_experiment(self):
+        queues = []
+        for i in range(self.action_size):
+            queues.append([])
+        for s in self.states:
+            if np.random.rand() >= self.epsilon:
                 action = self.random_argmax(self.states[s]['Q'])
-                queues[action].append(s)
-            
-            for i in range(self.action_size):
-                if len(queues[i]) != 0:
-                    self.last_state = random.choice(queues[i])
-                    self.last_action = i
-                    return self.last_state 
-        else:
-            self.last_action = np.random.randint(self.action_size)
-            self.last_state = random.choice(list(self.states.keys())) 
+            else: 
+                action = np.random.randint(self.action_size)
+            queues[action].append(s)
+        
+        for i in range(self.action_size):
+            if len(queues[i]) != 0:
+                self.last_state = random.choice(queues[i])
+                self.last_action = i
 
-            return self.last_state 
+                matching_injections = []
+                for key,value in self.fault_injections.items():
+                    if value == self.last_state:
+                        matching_injections.append(key)
+                fault_injection = random.choice(matching_injections)
+                return fault_injection
 
     def result(self, result):
         # Update Q
@@ -206,11 +326,26 @@ class TableauAlgorithm(BaseAlgorithm):
         indices = np.nonzero(vector == m)[0]
         return np.random.choice(indices)
 
+    def extract_features(self,operation,fault):
+        features = []
+
+        if operation.circuitbreaker != None:
+            features.append(1)
+        else:
+            features.append(0)
+        
+        features.append(len(operation.dependencies))
+
+        if fault == 'abort':
+            features.append(0)
+        else:
+            features.append(1)
+        return features
 
 class NeuralNetworkAlgorithm(BaseAlgorithm):
     def __init__(self,arch,params):
         super(NeuralNetworkAlgorithm, self).__init__(arch,params)
-        self.name = 'neuralnetwork'
+        self.name = 'ann'
         self.train_mode = True 
         self.experience_length = 10000
         self.experience_batch_size = 1000
@@ -238,7 +373,7 @@ class NeuralNetworkAlgorithm(BaseAlgorithm):
                                                       warm_start=warm_start, solver='adam', max_iter=750)
         self.model_fit = False
     
-    def get_action(self):
+    def get_experiment(self):
         probs = {}
         for state in self.states.keys():
             features = self.states[state]
@@ -336,10 +471,64 @@ class ExperienceReplay(object):
             batch = self.memory
 
         return batch
+
+class MLFQAlgorithm(BaseAlgorithm):
+    def __init__(self,arch,params):
+        super(MLFQAlgorithm, self).__init__(arch,params) 
+        self.name = 'mlfq'
+        self.queue_count = params[0]
+        self.iterations_reset = params[1]
+
+        self.init_queues()
+    
+    def get_experiment(self):
+        self.last_action = None 
+        while self.last_action == None:
+            q = np.random.choice(range(self.queue_count), 1, replace=True, p=self.probs)[0]
+            if not self.queues[q].empty():
+                self.last_action = self.queues[q].get() 
+                self.last_index = q
+                return self.last_action
+
+    def result(self,result):
+        if result > 0:
+            if self.last_index > 0:
+                self.last_index -= 1 
+        else:
+            if not self.last_index == self.queue_count - 1:
+                self.last_index += 1
+        
+        self.queues[self.last_index].put(self.last_action)
+
+        self.iterations += 1
+
+        if self.iterations == self.iterations_reset: 
+            self.init_queues()
+    
+    def init_queues(self):
+        self.iterations = 0
+        self.queues = [Queue()] * self.queue_count
+        operations = self.arch.get_operations() 
+        faults = ['abort','delay']
+        for op in operations:
+            if op.name not in ['a1','a2']:
+                for fault in faults:
+                    self.queues[0].put(op.name + '-' + fault)
+        self.probs = [] * self.queue_count 
+        max_prob = 100 
+        total = 0
+        for i in range(self.queue_count):
+            total = total + max_prob
+            self.probs.append(max_prob)
+            max_prob = max_prob/2
+        
+        probs_sum = sum(self.probs)
+        for i in range(len(self.probs)):
+            self.probs[i] = self.probs[i] / probs_sum
 class RandomAlgorithm(BaseAlgorithm):
     def __init__(self, arch,params):
         super(RandomAlgorithm, self).__init__(arch,params)
         self.name = 'random'
 
-    def get_action(self):
+    def get_experiment(self):
         return random.choice(self.fault_injections)
--- decisionengine/engine.py
+++ decisionengine/engine.py
@@ -38,7 +38,7 @@ class FaultInjector(object):
                             path = './data_service/' + 'a2' + '/' + self.date + '/' + patterns + '/' + faultinj +'.csv'
                             results_2 = pd.read_csv(path).sample(n=5000)
                             self.cached_results[patterns][faultinj] = {'a1': results_1, 'a2': results_2}
-    def lookup_result(self,architecture,faultinjection):
+    def inject_fault(self,architecture,faultinjection):
         patterns = architecture.patterns
         faults = 0
         for ip in ['a1','a2']:
@@ -67,29 +67,31 @@ def main():
         metrics['WFDR-R2'] = {}
         metrics['OPERATIONS-R1'] = {}
         metrics['OPERATIONS-R2'] = {} 
-        metrics['TIME'] = {}
+        metrics['TIME_GET'] = {}
+        metrics['TIME_RESULT'] = {}
         metrics['INIT_TIME'] = {} 
 
-        start_time = int(time.time() * 1000)
+        #start_time = int(time.time() * 1000)
         algorithms.append(RandomAlgorithm(archi,[]))
-        metrics['INIT_TIME']['random'] = int(time.time() * 1000) - start_time 
-        start_time = int(time.time() * 1000)
+        #metrics['INIT_TIME']['random'] = int(time.time() * 1000) - start_time 
+        #start_time = int(time.time() * 1000)
         algorithms.append(BanditEpsilonAlgorithm(archi,[0.5,0.99,0.1]))
-        metrics['INIT_TIME']['bandit-epsilon'] = int(time.time() * 1000) - start_time 
-        start_time = int(time.time() * 1000)
+        #metrics['INIT_TIME']['bandit-epsilon'] = int(time.time() * 1000) - start_time 
+        #start_time = int(time.time() * 1000)
         algorithms.append(BanditOptimisticAlgorithm(archi,[5]))
-        metrics['INIT_TIME']['bandit-optimistic'] = int(time.time() * 1000) - start_time 
-        start_time = int(time.time() * 1000)
-        algorithms.append(QLearningAlgorithm(archi,[0.2, 0.99, 0.66, 5]))
-        metrics['INIT_TIME']['qlearning'] = int(time.time() * 1000) - start_time
-
-        start_time = int(time.time() * 1000)
+        #metrics['INIT_TIME']['bandit-optimistic'] = int(time.time() * 1000) - start_time 
+        #start_time = int(time.time() * 1000)
+        #algorithms.append(QLearningAlgorithm(archi,[0.2, 0.99, 0.66, 5]))
+        #metrics['INIT_TIME']['qlearning'] = int(time.time() * 1000) - start_time
+        #start_time = int(time.time() * 1000)
         algorithms.append(TableauAlgorithm(archi,[0.4, 0.99, 5, 0.1,5]))
-        metrics['INIT_TIME']['tableau'] = int(time.time() * 1000) - start_time
-        start_time = int(time.time() * 1000)
+        #metrics['INIT_TIME']['tableau'] = int(time.time() * 1000) - start_time
+        #start_time = int(time.time() * 1000)
         algorithms.append(NeuralNetworkAlgorithm(archi,[12]))
-        metrics['INIT_TIME']['neuralnetwork'] = int(time.time() * 1000) - start_time
-
+        #metrics['INIT_TIME']['neuralnetwork'] = int(time.time() * 1000) - start_time
+        algorithms.append(BayesianLowAlgorithm(archi,[]))
+        algorithms.append(BayesianHighAlgorithm(archi,[])) 
+        algorithms.append(MLFQAlgorithm(archi,[5,200]))
         pool = ThreadPool(len(algorithms))
         pool.map(partial(run_experiment,archi,metrics,mocker), algorithms)
         pool.close()
@@ -99,9 +101,9 @@ def main():
         generate_ouptput(metrics,archi)
 
 def run_experiment(arch,metrics,mocker,algorithm):
-    runs = 10
-    faults_to_find = 1000
-    experiments_to_run = 2000 # 5000
+    runs = 50
+    faults_to_find = 500
+    experiments_to_run = 500 # 5000
 
 
     total = (runs * (faults_to_find + experiments_to_run)) * 2
@@ -111,22 +113,24 @@ def run_experiment(arch,metrics,mocker,algorithm):
     injection_selections_R1 = []
     injection_results_R2 = []
     injection_selections_R2 = []
-
+    injection_times_get = []
+    injection_times_result = []
     for run in range(runs):
         injection_results_R1.append({'ER': [], 'WFDR': []})
         injection_results_R2.append({'ER': [], 'WFDR': []})
 
         injection_selections_R1.append({'ER': {}, 'WFDR': {}})
         injection_selections_R2.append({'ER': {}, 'WFDR': {}})
-        
+        injection_times_get.append([])
+        injection_times_result.append([])
         i = 0
         while i < faults_to_find:
-            fault_injection = algorithm.get_action()
+            fault_injection = algorithm.get_experiment()
             if fault_injection in injection_selections_R1[run]['ER'].keys():
                 injection_selections_R1[run]['ER'][fault_injection] += 1
             else: 
                 injection_selections_R1[run]['ER'][fault_injection] = 0 
-            result = mocker.lookup_result(arch,fault_injection)
+            result = mocker.inject_fault(arch,fault_injection)
             injection_results_R1[run]['ER'].append(result)
 
             if result > 0:
@@ -141,12 +145,12 @@ def run_experiment(arch,metrics,mocker,algorithm):
 
         i = 0
         while i < faults_to_find:
-            fault_injection = algorithm.get_action()
+            fault_injection = algorithm.get_experiment()
             if fault_injection in injection_selections_R2[run]['ER'].keys():
                 injection_selections_R2[run]['ER'][fault_injection] += 1
             else: 
                 injection_selections_R2[run]['ER'][fault_injection] = 0 
-            result = mocker.lookup_result(arch,fault_injection)
+            result = mocker.inject_fault(arch,fault_injection)
             injection_results_R2[run]['ER'].append(result)
             if result > 0:
                 done += 1
@@ -157,29 +161,33 @@ def run_experiment(arch,metrics,mocker,algorithm):
         algorithm.reset() 
 
         for i in range(experiments_to_run):
-            fault_injection = algorithm.get_action()
+            start_time = int(time.time() * 1000)
+            fault_injection = algorithm.get_experiment()
+            injection_times_get[run].append(int(time.time() * 1000) - start_time)
             if fault_injection in injection_selections_R1[run]['WFDR'].keys():
                 injection_selections_R1[run]['WFDR'][fault_injection] += 1
             else: 
                 injection_selections_R1[run]['WFDR'][fault_injection] = 0 
-            result = mocker.lookup_result(arch,fault_injection)
+            result = mocker.inject_fault(arch,fault_injection)
             injection_results_R1[run]['WFDR'].append(result)
+            start_time = int(time.time() * 1000)
             if result > 0:
                 algorithm.result(1)
             else:
                 algorithm.result(0)
+            injection_times_result[run].append(int(time.time() * 1000) - start_time)
             done += 1
             if done % 100 == 0:
                 print("Progress (" + algorithm.name + "):" + str(round((done / total) * 100,2)) + "%")
         algorithm.reset() 
 
         for i in range(experiments_to_run):
-            fault_injection = algorithm.get_action()
+            fault_injection = algorithm.get_experiment()
             if fault_injection in injection_selections_R2[run]['WFDR'].keys():
                 injection_selections_R2[run]['WFDR'][fault_injection] += 1
             else: 
                 injection_selections_R2[run]['WFDR'][fault_injection] = 0 
-            result = mocker.lookup_result(arch,fault_injection)
+            result = mocker.inject_fault(arch,fault_injection)
             injection_results_R2[run]['WFDR'].append(result)
             algorithm.result(result)
             done += 1
@@ -195,6 +203,8 @@ def run_experiment(arch,metrics,mocker,algorithm):
     metrics['WFDR-R2'][algorithm.name] = compute_WFDR(injection_results_R2,experiments_to_run) 
     metrics['OPERATIONS-R1'][algorithm.name] = compute_operations_counter(injection_selections_R1)
     metrics['OPERATIONS-R2'][algorithm.name] = compute_operations_counter(injection_selections_R2)
+    metrics['TIME_GET'][algorithm.name] = compute_time(injection_times_get)
+    metrics['TIME_RESULT'][algorithm.name] = compute_time(injection_times_result)
 
 def compute_ER(data,limit):
     thresholds = [] # [50,200,500,1000,2000]
@@ -265,9 +275,17 @@ def compute_WFDR(data,total):
                 if i+1 in thresholds:
                     result[i+1].append(counter)
             
-
-
     return result 
+
+def compute_time(data):
+    total_sum = 0
+    total_len = 0
+    
+    for run in data:
+        total_len += len(run)
+        total_sum += sum(run) 
+    
+    return total_sum / total_len
 
 def compute_operations_counter(data):
     result = []
@@ -282,7 +300,8 @@ def generate_ouptput(metrics,archi):
     FDR2 = metrics['FDR-R2']
     WFDR1 = metrics['WFDR-R1']
     WFDR2 = metrics['WFDR-R2']
-    TIME = metrics['TIME']
+    TIME_GET = metrics['TIME_GET']
+    TIME_RESULT = metrics['TIME_RESULT']
     INIT_TIME = metrics['INIT_TIME']
     OPERATIONS1 = metrics['OPERATIONS-R1']
     OPERATIONS2 = metrics['OPERATIONS-R2']
@@ -291,6 +310,9 @@ def generate_ouptput(metrics,archi):
 
     visualize_fdr([FDR1,FDR2],archi,'FDR')
     visualize_fdr([WFDR1,WFDR2],archi,'WFDR')
+
+    visualize_time(TIME_GET, 'average time for get_experiment()')
+    visualize_time(TIME_RESULT, 'average time for result()')
 
     table_counter([OPERATIONS1, OPERATIONS2], archi)
     # what to do:
@@ -319,7 +341,7 @@ def visualize_er(data,archi):
         plt.xlabel('faults_found')
         plt.ylabel('experiments_run') 
         plt.legend(loc='best')
-        path = './figures/' + patterns + '-' + str(i+1) + '-ER-line.png' 
+        path = './figures-noq/' + patterns + '-' + str(i+1) + '-ER-line.png' 
         plt.savefig(path)
         plt.close()
 
@@ -337,13 +359,13 @@ def visualize_fdr(data,archi,yaxis):
         plt.xlabel('experiments_run')
         plt.ylabel(yaxis) 
         plt.legend(loc='best')
-        path = './figures/' + patterns + '-' + str(i+1) + '-' + yaxis + '-line.png'
+        path = './figures-noq/' + patterns + '-' + str(i+1) + '-' + yaxis + '-line.png'
         plt.savefig(path)
         plt.close()
 
 
 
-        labels = data[i].keys()
+        labels = sorted(data[i].keys())
         data_all = []
         for algorithm in labels:
             max_key = max(data[i][algorithm].keys())
@@ -357,17 +379,38 @@ def visualize_fdr(data,archi,yaxis):
         ax.xaxis.set_ticks_position('none')
         ax.yaxis.set_ticks_position('none')
         ax.xaxis.set_ticklabels(labels)
+        plt.xticks(rotation=90)
+        plt.gcf().subplots_adjust(bottom=0.25)
+
 
 
         ax.yaxis.set_label_text(yaxis + ' for ' + str(max_key) + ' experiments')
         ax.set_title(yaxis + ' for ' + patterns + ', reward function ' + str(i+1))
         formatter = mtick.ScalarFormatter(useOffset=False)
         ax.yaxis.set_major_formatter(formatter)
-        path = './figures/' + patterns + '-' + str(i+1) + '-' + yaxis + '-box.png'
+        path = './figures-noq/' + patterns + '-' + str(i+1) + '-' + yaxis + '-box.png'
         plt.savefig(path)
         plt.close()
         
+def visualize_time(data,title):
+    xvals = sorted(data.keys())
+    yvals = [] 
+    for x in xvals:
+        yvals.append(data[x])
+    
+    plt.bar(xvals,yvals) 
+    plt.xticks(rotation=90)
+    plt.ylabel('ms')
+    plt.title(title)
+    plt.gcf().subplots_adjust(bottom=0.2)
 
+    if 'experiment' in title:
+        path = './figures-noq/times_get.png' 
+    else: 
+        path = './figures-noq/times_result.png'
+        
+    plt.savefig(path)
+    plt.close() 
 def table_counter(data, archi):
     patterns = archi.patterns 
 
@@ -393,7 +436,7 @@ def table_counter(data, archi):
         
         csv_columns = ['fault-injection'] + algorithms 
 
-        csv_file = './figures/' + patterns + '-' + str(i+1) + '-counter.csv'
+        csv_file = './figures-noq/' + patterns + '-' + str(i+1) + '-counter.csv'
         try:
             with open(csv_file, 'w') as csvfile:
                 writer = csv.writer(csvfile, delimiter = ' ')
